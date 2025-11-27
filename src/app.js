d4c4433dcd71c0b0a19ebd88e4d38d3a3978a2f1
vanshYadav <vanshajjyadava@gmail.com>
2025-11-27 09:57:13 +0530

    created an express server.

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the server.");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello from dev-Tinder.");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port no. 3000");
+});
