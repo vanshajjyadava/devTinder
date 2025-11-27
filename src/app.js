df54a91f129845abf2db46e43478dcba1ed11c85
vanshYadav <vanshajjyadava@gmail.com>
2025-11-27 17:53:33 +0530

    multiple-route-handles

--- src/app.js
+++ src/app.js
@@ -2,20 +2,37 @@ const express = require("express");
 
 const app = express();
 
-// '?' means that the letter "b" can exist or not.
-app.get(/ab?c/, (req, res) => {
-  res.send({ firstName: "Vansh", lastName: "Yadav" });
-});
-
-// '+' means that the letter "b" can repeat inself multiple times.
-app.get(/ab+c/, (req, res) => {
-  res.send({ firstName: "Vansh", lastName: "Yadav" });
-});
-
-// '*' means that you could write anything in b/w ab and cd, it will work.
-app.get("/ab*cd/", (req, res) => {
-  res.send({ firstName: "Vansh", lastName: "Yadav" });
-});
+app.use("/user", [
+  (req, res, next) => {
+    // Route Handler 1
+    console.log("Route handler 1");
+    // res.send("Response 1");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Route Handler 2");
+    // res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 3
+    console.log("Route Handler 3");
+    // res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 4
+    console.log("Route Handler 4");
+    // res.send("Response 4");
+    next();
+  },
+  (req, res) => {
+    // Route Handler 5
+    console.log("Route Handler 5");
+    res.send("Response 5");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port no. 3000");
