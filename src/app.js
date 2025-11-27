7e637043e8f4a579187b717d80053b1724377533
vanshYadav <vanshajjyadava@gmail.com>
2025-11-27 10:19:20 +0530

    Added / path

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res) => {
+  res.send("Namaste Vansh");
+});
+
 app.use("/test", (req, res) => {
   res.send("Hello from the server.");
 });
