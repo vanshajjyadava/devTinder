52ec24f766f10ebc1aa9c25355aa233f2c176c0e
vanshYadav <vanshajjyadava@gmail.com>
2025-12-01 17:07:22 +0530

    dynamic sign-up

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ const User = require("./models/user");
 
 const app = express();
 
+// Converts JSON into JS obj
+app.use(express.json());
+
 app.post("/signup", async (req, res, next) => {
   // Creating a new instance of the user model.
-  const user = new User({
-    firstName: "Vansh",
-    lastName: "Yadav",
-    emailId: "vanshyadav@gmail.com",
-    password: "Vansh@123",
-  });
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully.");
